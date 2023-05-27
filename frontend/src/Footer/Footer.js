79fb414a932bc87c1daf3b96e073968aaefb9179
Akula Vasuteja <vasutej249@gmail.com>
2023-05-27 21:41:57 +0530

    first commit

---
+++ frontend/src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="text-center text-lg-start">
+      <div className="mt-3">
+        <div className="row align-items-center">
+          <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <p className="text-uppercase colorDesign footer-p">ProjectTrack</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
